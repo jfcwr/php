a7235a98122fdae7302cee722c8a53b520c4fcc2
brook <brook@qq.com>
2023-04-21 11:07:06 +0800

    update

---
+++ fish/fishcenter/saveresult.php
@@ -0,0 +1,17 @@
+<?php
+	header('content-type:application:json;charset=utf8'); 
+	header('Access-Control-Allow-Origin:*'); 
+	header('Access-Control-Allow-Methods:POST'); 
+	header('Access-Control-Allow-Headers:x-requested-with,content-type'); 
+	date_default_timezone_set('PRC');
+	$filecontent = file_get_contents("php://input");
+	$fp = fopen("result.csv", 'w');
+	fwrite($fp, $filecontent);
+	fclose($fp);
+
+	$curTime = date("ymd_his");
+	$fp = fopen("result_".$curTime.".csv", 'w');
+	fwrite($fp, $filecontent);
+	fclose($fp);
+	echo("succeed!");
+?>
